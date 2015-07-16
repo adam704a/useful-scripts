bb250fbf0d19423de5a023b250bf8e8e7d95fdad
Adam Preston <adam.m.preston@gmail.com>
2015-07-16 12:05:58 -0600

    format dates as datetime

--- twitter_getter.py
+++ twitter_getter.py
@@ -316,6 +316,9 @@ class TweetSaver():
                             )
             if use_mongo:
                 
+                formatted_date = datetime.datetime.strptime(data['created_at'], '%a %b %d %X %z %Y')
+                data['created_at'] = formatted_date
+
                 # Push the JSON into MongoDB
                 collection.insert(data)
 
